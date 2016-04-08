c1d76a8d902d41903df27458d0b80697ad20a50d
Zach <ztrichas2019@pingry.org>
2016-04-07 22:29:22 -0400

    Added test cases for 0

--- ps0-runner.py
+++ ps0-runner.py
@@ -8,6 +8,10 @@ print(ps0.is_even(14))
 print("Function 0")
 print(ps0.is_even(15))
 
+#Function 0 C
+print("Function 0")
+print(ps0.is_even(0))
+
 #Function 1 A
 print("Function 1")
 print(ps0.length(143))
@@ -15,6 +19,10 @@ print(ps0.length(143))
 #Function 1 B
 print("Function 1")
 print(ps0.length(23142567))
+
+#Function 1 C
+print("Function 1")
+print(ps0.length(0))
 
 #Function 2 A
 print("Function 2")
@@ -24,6 +32,10 @@ print(ps0.sum_digits(52))
 print("Function 2")
 print(ps0.sum_digits(143))
 
+#Function 2 C
+print("Function 2")
+print(ps0.sum_digits(0))
+
 #Function 3 A
 print("Function 3")
 print(ps0.sum_of_lesser_ints(5))
@@ -32,6 +44,9 @@ print(ps0.sum_of_lesser_ints(5))
 print("Function 3")
 print(ps0.sum_of_lesser_ints(7))
 
+#Function 3 C
+print("Function 3")
+print(ps0.sum_of_lesser_ints(0))
 
 #Function 4 A
 print("Function 4")
@@ -41,6 +56,10 @@ print(ps0.factorial(5))
 print("Function 4")
 print(ps0.factorial(7))
 
+#Function 4 C
+print("Function 4")
+print(ps0.factorial(0))
+
 #Function 5 A
 print("Function 5")
 print(ps0.is_it_a_factor(2, 4))
@@ -48,6 +67,14 @@ print(ps0.is_it_a_factor(2, 4))
 #Function 5 B
 print("Function 5")
 print(ps0.is_it_a_factor(2, 3))
+
+#Function 5 C
+print("Function 5")
+print(ps0.is_it_a_factor(0, 1))
+
+#Function 5 D
+print("Function 5")
+print(ps0.is_it_a_factor(1, 0))
 
 #Function 6 B
 print("Function 6")
@@ -57,6 +84,10 @@ print(ps0.is_it_prime(5))
 print("Function 6")
 print(ps0.is_it_prime(6))
 
+#Function 6 C
+print("Function 6")
+print(ps0.is_it_prime(0))
+
 #Function 7 A
 print("Function 7")
 print(ps0.is_it_perfect(6))
@@ -65,6 +96,10 @@ print(ps0.is_it_perfect(6))
 print("Function 7")
 print(ps0.is_it_perfect(7))
 
+#Function 7 C
+print("Function 7")
+print(ps0.is_it_perfect(0))
+
 #Function 8 A
 print("Function 8")
 print(ps0.sum_divided_by_number(624))
@@ -72,3 +107,7 @@ print(ps0.sum_divided_by_number(624))
 #Function 8 B
 print("Function 8")
 print(ps0.sum_divided_by_number(2134))
+
+#Function 8 C
+print("Function 8")
+print(ps0.sum_divided_by_number(0))
